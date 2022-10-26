864c3f642deedab55be43aa986a861a9394058a5
susmitha <susmithareddychinthala024@gmail.com>
2022-10-26 14:10:59 +0530

    Added: Code for the bet to win or loose the Gambler

--- src/com/gamblingmethod/GamblerMainMethod.java
+++ src/com/gamblingmethod/GamblerMainMethod.java
@@ -1,8 +1,23 @@
 package com.gamblingmethod;
 
+import java.util.Random;
+
 public class GamblerMainMethod {
     public static void main(String[] args) {
         GamblerSimulation gambler = new GamblerSimulation();
         System.out.println(gambler.toString());
+        Random rand = new Random();
+        int random = rand.nextInt(2);
+        winOrLoose(random, gambler.getBetValue());
+    }
+
+    public static void winOrLoose(int random, int betValue) {
+
+        if (random == betValue) {
+            System.out.println("Gambler wins!!!");
+        } else {
+            System.out.println("Gambler Loose!!!");
+        }
+
     }
 }
